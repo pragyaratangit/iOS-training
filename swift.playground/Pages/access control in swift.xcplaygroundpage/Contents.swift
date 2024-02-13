011da21a87b49861655f37c309269df539791e64
Pragyaratan <pragyaratan.oyo@oyorooms.com>
2024-02-13 11:15:33 +0530

    Opaque type

--- swift.playground/Pages/access control in swift.xcplaygroundpage/Contents.swift
+++ swift.playground/Pages/access control in swift.xcplaygroundpage/Contents.swift
@@ -14,3 +14,59 @@ var greeting = "Hello, playground"
 //File-private access restricts the use of an entity to its own defining source file. Use file-private access to hide the implementation details of a specific piece of functionality when those details are used within an entire file.
 
 //Private access restricts the use of an entity to the enclosing declaration, and to extensions of that declaration that are in the same file. Use private access to hide the implementation details of a specific piece of functionality when those details are used only within a single declaration.
+
+
+/*
+open class SomeOpenClass {}
+public class SomePublicClass {}
+internal class SomeInternalClass {}
+fileprivate class SomeFilePrivateClass {}
+private class SomePrivateClass {}
+*/
+//Only classes and overridable class members can be declared 'open'
+//open var someOpenVariable = 0
+
+public var somePublicVariable = 0
+internal let someInternalConstant = 0
+fileprivate func someFilePrivateFunction() {}
+private func somePrivateFunction() {}
+
+
+
+public class SomePublicClass {                  // explicitly public class
+    public var somePublicProperty = 0            // explicitly public class member
+    var someInternalProperty = 0                 // implicitly internal class member
+    fileprivate func someFilePrivateMethod() {}  // explicitly file-private class member
+    private func somePrivateMethod() {}          // explicitly private class member
+}
+
+
+class SomeInternalClass {                       // implicitly internal class
+    var someInternalProperty = 0                 // implicitly internal class member
+    fileprivate func someFilePrivateMethod() {}  // explicitly file-private class member
+    private func somePrivateMethod() {}          // explicitly private class member
+}
+
+
+fileprivate class SomeFilePrivateClass {        // explicitly file-private class
+    func someFilePrivateMethod() {}              // implicitly file-private class member
+    private func somePrivateMethod() {}          // explicitly private class member
+}
+
+
+private class SomePrivateClass {                // explicitly private class
+    func somePrivateMethod() {}                  // implicitly private class member
+//    public var property = 1                   // should be avoided
+}
+
+// this wont compile becoz someFunction has a access of internal but one of two arguments(SomePrivateClass) has private access
+// we must mark function private(lower access)
+private func someFunction() -> (SomeInternalClass, SomePrivateClass) {
+    // function implementation goes here
+    return (SomeInternalClass(), SomePrivateClass())
+}
+
+
+//private var obj = SomePrivateClass()
+//
+//print(obj.property)
---
+++ swift.playground/Pages/opaque type.xcplaygroundpage/Contents.swift
@@ -0,0 +1,109 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
+
+//An opaque type lets the function implementation pick the type for the value it returns in a way that’s abstracted away from the code that calls the function.
+protocol SomeProtocol {
+    associatedtype MyType
+    var p1:MyType {get set}
+}
+
+
+class Class1: SomeProtocol {
+    var p1:Int = 100
+}
+class Class2: SomeProtocol {
+    var p1:Int = 200
+}
+
+
+
+// SomeProtocol uses associated type
+// Class1 and Class2 both conforms to SomeProtocol
+// someFun funtion will confuse wheather return an object of Class1 or Class2
+// so its required to use opaque type
+// now function can return any class that conforms to SomeProtocol
+func someFun(flag: Bool) ->some SomeProtocol {
+    return flag ? Class1() : Class1()
+}
+someFun(flag: false)
+
+
+
+// Example
+protocol Shape {
+    func draw() -> String
+}
+
+
+struct Triangle: Shape {
+    var size: Int
+    func draw() -> String {
+       var result: [String] = []
+       for length in 1...size {
+           result.append(String(repeating: "*", count: length))
+       }
+       return result.joined(separator: "\n")
+    }
+}
+let smallTriangle = Triangle(size: 3)
+print(smallTriangle.draw())
+
+
+struct FlippedShape<T: Shape>: Shape {
+    var shape: T
+    func draw() -> String {
+        let lines = shape.draw().split(separator: "\n")
+        return lines.reversed().joined(separator: "\n")
+    }
+}
+let flippedTriangle = FlippedShape(shape: smallTriangle)
+print(flippedTriangle.draw())
+
+
+struct JoinedShape<T: Shape, U: Shape>: Shape {
+    var top: T
+    var bottom: U
+    func draw() -> String {
+       return top.draw() + "\n" + bottom.draw()
+    }
+}
+let joinedTriangles = JoinedShape(top: smallTriangle, bottom: flippedTriangle)
+print(joinedTriangles.draw())
+
+
+struct Square: Shape{
+    var size: Int
+    
+    func draw() -> String {
+        var results: [String] = []
+                   
+        for _ in 1...size {
+            results.append(String(repeating: "*", count: size))
+        }
+        return results.joined(separator: "\n")
+    }
+}
+
+var sizeThreeSquare = Square(size: 3).draw()
+print(sizeThreeSquare)
+
+
+// returning opaque type
+
+func makeTrapezoid() ->  some Shape {
+    let top = Triangle(size: 4)
+    let middle = Square(size: 4)
+    let bottom = FlippedShape(shape: top)
+    let trapezoid = JoinedShape(
+        top: top,
+        bottom: JoinedShape(top: middle, bottom: bottom)
+    )
+    return trapezoid
+}
+let trapezoid = makeTrapezoid()
+print(trapezoid.draw())
