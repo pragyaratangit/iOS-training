67a39f52bb877d7946c0b08b331c500602db1fbb
Pragyaratan <pragyaratan.oyo@oyorooms.com>
2024-02-21 11:25:45 +0530

    Aspect Ratio

---
+++ UIKit/aspect ratio/aspect ratio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aspect ratio
+//
+//  Created by Pragyaratan on 20/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
