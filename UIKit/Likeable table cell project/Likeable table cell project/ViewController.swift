336986c32ee0aca80ea50ada4b79ee69dea47439
Pragyaratan <pragyaratan.oyo@oyorooms.com>
2024-02-29 13:15:02 +0530

    Table view

--- UIKit/Likeable table cell project/Likeable table cell project/ViewController.swift
+++ UIKit/Likeable table cell project/Likeable table cell project/ViewController.swift
@@ -7,100 +7,94 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    var data = ["first", "second", "third", "fouth", "fifth"]
-//    var favData:[String] = []
-   
-//    var refToFavClass: FavouriteTab?
-    @IBOutlet weak var table: UITableView!
-   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-//        view.addSubview(table)
-        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        
-        table.dataSource = self
-        table.delegate = self
-//        refToFavClass = FavouriteTab()
-        
+class LikeableItem{
+    var text: String
+    var isLiked = false
+    
+    init(text: String) {
+        self.text = text
     }
-
-//    override func viewDidLayoutSubviews() {
-//        super.viewDidLayoutSubviews()
-//        table.frame = view.bounds
-//        favTable.frame = v
-//    }
-    
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       
-        
-        
-        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        cell.imageView?.image = UIImage(systemName: "heart")
-        
-        cell.textLabel?.text = data[indexPath.row]
-        return cell
-    }
-    
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        let cell = tableView.cellForRow(at: indexPath)
-        
-        if cell?.imageView?.image == UIImage(systemName: "heart.fill"){
-            cell?.imageView?.image = UIImage(systemName: "heart")
-//            refToFavClass?.favData.removeAll(where: {
-//                text in
-//                text == cell?.textLabel?.text
-//            })
-        }
-        else{
-            cell?.imageView?.image = UIImage(systemName: "heart.fill")
-//            refToFavClass?.favData.append(data[indexPath.row])
-        }
-        
-        
-    }
-    
 }
 
 
-
-class FavouriteTab: UIViewController ,UITableViewDelegate, UITableViewDataSource  {
-   
-    public var favData: [String] = ["abc", "bcd", "def"]
+class DataClass {
+    var items: [LikeableItem] = ["first","Second", "Third", "Fourth"].compactMap({LikeableItem(text: $0)})
     
-    @IBOutlet weak var table: UITableView!
+    
+    func getData() -> [LikeableItem] {
+        return items
+    }
+    func getLikedData() -> [LikeableItem] {
+        var arr = [LikeableItem]()
+        
+        for item in items {
+            if item.isLiked {
+                arr.append(item)
+            }
+        }
+        return arr
+    }
+     func addData(text: String) {
+        items.append(LikeableItem(text: text))
+    }
+}
+
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    let items:[LikeableItem] = DataClass().getData()
+   
+    private let table: UITableView = {
+        let table = UITableView()
+        
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return table
+    }()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(table)
-        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        
         table.dataSource = self
         table.delegate = self
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        table.frame = view.bounds
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return favData.count
+        return items.count
+        
+        
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let item = items[indexPath.row]
+        table.deselectRow(at: indexPath, animated: true)
+        item.isLiked = !item.isLiked
+        
+        table.reloadRows(at: [indexPath], with: .automatic)
+        
+    }
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
-        cell.textLabel?.text = favData[indexPath.row]
+        let item = items[indexPath.row]
+        
+        cell.textLabel?.text =  item.text
+        
+        cell.imageView?.image = item.isLiked ? UIImage(systemName: "heart.fill") : UIImage(systemName: "heart")
         
         return cell
     }
+    
+    
 }
+
