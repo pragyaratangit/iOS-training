275a8c48448291956f2950ce3eae3b2de1509b39
Pragyaratan <pragyaratan.oyo@oyorooms.com>
2024-03-01 16:01:47 +0530

    Vertical scroll view

---
+++ UIKit/scroll view/scroll view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  scroll view
+//
+//  Created by Pragyaratan on 01/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
